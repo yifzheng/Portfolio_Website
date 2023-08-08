976c955a8779c1a98420189e4d287961bb2a2a2a
Yifeng Zheng <yifeng.zheng43@bcmail.cuny.edu>
2023-08-08 16:00:23 -0400

    finished sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Works from "./components/works/Works"
 import Contact from "./components/contact/Contact"
 import './app.scss'
 import { useState } from "react"
+import Menu from "./components/menu/Menu"
 
 function App () {
   const [ menuOpen, setMenuOpen ] = useState( false ) // menu initially not opened
@@ -13,6 +14,7 @@ function App () {
   return (
     <div className="app">
       <Topbar menuOpen={ menuOpen } setMenuOpen={ setMenuOpen } />
+      <Menu menuOpen={ menuOpen } setMenuOpen={ setMenuOpen } />
       <div className="sections">
         <Intro />
         <Portfolio />
