0f73b9517bf60a1516567de3c4b11485eb2c2483
Yifeng Zheng <yifeng.zheng43@bcmail.cuny.edu>
2023-08-08 21:30:37 -0400

    finished template portfolio

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,8 +1,21 @@
 import './Intro.scss'
 import Profile from "../../assets/profile.png"
 import DownArrow from "../../assets/down-arrow.png"
+import { init } from "ityped"
+import { useEffect, useRef } from 'react'
 
 const Intro = () => {
+  const textRef = useRef()
+
+  useEffect( () => {
+    init( textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60, 
+      strings: [ "Engineer", "Developer" ],
+    } )
+  }, [] )
+
   return (
     <div className='intro' id='intro'>
       <div className="left">
@@ -14,7 +27,7 @@ const Intro = () => {
         <div className="wrapper">
           <h2>Hi There, I&#39;m</h2>
           <h1>Yifeng Zheng</h1>
-          <h3>Aspiring Software <span></span></h3>
+          <h3>Aspiring Software <span ref={ textRef }></span></h3>
         </div>
         <a href="#portfolio">
           <img src={ DownArrow } alt="" />
