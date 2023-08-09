777e9dc0954c67fc09a4c75f90a254b9669cf394
Yifeng Zheng <yifeng.zheng43@bcmail.cuny.edu>
2023-08-09 18:25:27 -0400

    updated the image links from data file

--- src/data.js
+++ src/data.js
@@ -1,40 +1,46 @@
 import PandaChat from "./assets/landing.png";
+import DeerDiary from "./assets/deer_diary.png";
+import Aestimogram from "./assets/landing-page.png";
+import Weather from "./assets/weather_prediction.png";
+import Nature from "./assets/nature_duathalon.png";
+import Calculator from "./assets/calculator.png";
+import Todo from "./assets/to_do.png";
 
 export const featuredPortfolio = [
 	{
 		id: 1,
+		title: "Aestimogram",
+		img: Aestimogram,
+		url: "https://aestimogram.netlify.app/",
+	},
+	{
+		id: 2,
 		title: "Panda Chat",
 		img: PandaChat,
 		url: "https://ppandachatt.netlify.app/",
 	},
 	{
-		id: 2,
-		title: "Aestimogram",
-		img: "/src/assets/landing-page.png",
-		url: "https://aestimogram.netlify.app/",
-	},
-	{
 		id: 3,
 		title: "Deer Diary",
-		img: "/src/assets/deer_diary.png",
+		img: DeerDiary,
 		url: "https://github.com/yifzheng/DeerDiary",
 	},
 	{
 		id: 4,
 		title: "Weather Prediction",
-		img: "/src/assets/weather_prediction.png",
+		img: Weather,
 		url: "https://github.com/yifzheng/Machine-Learning-Weather-Prediction",
 	},
 	{
 		id: 5,
 		title: "Nature's Duathalon",
-		img: "/src/assets/nature_duathalon.png",
+		img: Nature,
 		url: "https://yifzheng.itch.io/natures-duathalon",
 	},
 	{
 		id: 6,
 		title: "Android Calculator",
-		img: "/src/assets/calculator.png",
+		img: Calculator,
 		url: "https://github.com/yifzheng/Android-Applications/tree/master/Calculator",
 	},
 ];
@@ -49,13 +55,13 @@ export const webPortfolio = [
 	{
 		id: 2,
 		title: "Aestimogram",
-		img: "/src/assets/landing-page.png",
+		img: Aestimogram,
 		url: "https://aestimogram.netlify.app/",
 	},
 	{
 		id: 3,
 		title: "ToDo Web App",
-		img: "/src/assets/to_do.png",
+		img: Todo,
 		url: "https://yifzheng.github.io/Todo-Frontend/",
 	},
 ];
@@ -64,13 +70,13 @@ export const mobilePortfolio = [
 	{
 		id: 1,
 		title: "Deer Diary",
-		img: "/src/assets/deer_diary.png",
+		img: DeerDiary,
 		url: "https://github.com/yifzheng/DeerDiary",
 	},
 	{
 		id: 2,
 		title: "Android Calculator",
-		img: "/src/assets/calculator.png",
+		img: Calculator,
 		url: "https://github.com/yifzheng/Android-Applications/tree/master/Calculator",
 	},
 ];
