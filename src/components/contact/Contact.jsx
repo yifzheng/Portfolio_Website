083e5933050aaa9cee5a457b628b073f9fead154
Yifeng Zheng <yifeng.zheng43@bcmail.cuny.edu>
2023-08-09 17:47:19 -0400

    undo

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,11 +2,34 @@ import { useState } from "react"
 import "./Contact.scss"
 
 const Contact = () => {
-    const [ message, setMessage ] = useState( false )
+    const [ send, setSend ] = useState( '' )
+    const [ email, setEmail ] = useState( "" )
+    const [ message, setMessage ] = useState( "" )
 
+    // handle the submission of form and send email
     const handleSubmit = ( e ) => {
         e.preventDefault()
-        setMessage( true )
+
+        fetch( "/send-email", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify( { email, message } )
+        } )
+            .then( response => response.json() )
+            .then( data => {
+                if ( data.success ) {
+                    setSend( "Email sent successfully" )
+                }
+                else {
+                    setSend( "Email could not be sent" )
+                }
+            } )
+            .catch( error => {
+                console.error( error )
+                setSend( 'An error occurred' )
+            } )
     }
 
     return (
@@ -17,10 +40,26 @@ const Contact = () => {
             <div className="right">
                 <h2>Contact.</h2>
                 <form onSubmit={ handleSubmit }>
-                    <input type="text" name="email" id="email" placeholder="Email" />
-                    <textarea name="message" id="message" cols="30" rows="10"></textarea>
+                    <input
+                        type="text"
+                        name="email"
+                        id="email"
+                        placeholder="Email"
+                        value={ email }
+                        onChange={ ( e ) => setEmail( e.target.value ) }
+                        required
+                    />
+                    <textarea
+                        name="message"
+                        id="message"
+                        cols="30"
+                        rows="10"
+                        value={ message }
+                        onChange={ ( e ) => setMessage( e.target.value ) }
+                        required
+                    ></textarea>
                     <button type="submit" >Send</button>
-                    {message && <span>Thanks, I&#39;ll respond ASAP</span>}
+                    { send.length > 0 && <span>Thanks, I&#39;ll respond ASAP</span> }
                 </form>
             </div>
         </div>
